a0b75b37a16898e849833b9af3afb9d410489d01
Rojo-Web <santifa2004@gmail.com>
2023-11-04 23:30:00 -0500

    Create  Categories

--- SupermarketWeb/Models/Category.cs
+++ SupermarketWeb/Models/Category.cs
@@ -6,7 +6,7 @@
         public string Name { get; set; }
         public string? Description { get; set; }
 
-        public ICollection<Product> Products { get; set; } //Propiedad de navegacion
+        public ICollection<Product>? Products { get; set; } = default!; //Propiedad de navegacion
 
     }
 }
---
+++ SupermarketWeb/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWeb.Data;
+using SupermarketWeb.Models;
+
+namespace SupermarketWeb.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SumpermarketContext _Context;
+
+        public IndexModel(SumpermarketContext context)
+        {
+            _Context = context;
+        }
+
+        public IList<Category> Categories { get; set; } = default!;
+        public async Task OnGetAsync()
+        {
+
+            if(_Context.Categories != null)
+            {
+                Categories = await _Context.Categories.ToListAsync();
+            }
+
+        }
+    }
+}
