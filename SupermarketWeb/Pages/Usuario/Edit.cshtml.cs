ae4c183713fe5c56cd4d8f39dbf0ee50945aa1da
Rojo-Web <santifa2004@gmail.com>
2023-11-12 02:14:24 -0500

    Edit de user

---
+++ SupermarketWeb/Pages/Usuario/Edit.cshtml.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWeb.Data;
+using SupermarketWeb.Models;
+
+namespace SupermarketWeb.Pages.Usuario
+{
+    public class EditModel : PageModel
+    {
+        private readonly SumpermarketContext _context;
+
+        public EditModel(SumpermarketContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+
+        public User Users { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            Users = user;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Users).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsersExists(Users.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToPage("./Index");
+        }
+        private bool UsersExists(int id)
+        {
+            return (_context.Users?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
