4bfa354ffd44370a5de454c994021556cfaa92c7
Rojo-Web <santifa2004@gmail.com>
2023-11-12 00:17:38 -0500

    eliminar paymode y organizacion de botones del header

---
+++ SupermarketWeb/Pages/PayModee/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWeb.Data;
+using SupermarketWeb.Models;
+
+namespace SupermarketWeb.Pages.PayModee
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly SumpermarketContext _context;
+
+        public DeleteModel(SumpermarketContext context)
+        {
+            _context = context;
+        }
+        [BindProperty]
+        public PayMode PayMode { get; set; } = default;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.PayModes == null)
+            {
+                return NotFound();
+
+            }
+            var paymode = await _context.PayModes.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (paymode == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                PayMode = paymode;
+
+            }
+            return Page();
+
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.PayModes == null)
+            {
+                return NotFound();
+            }
+            var paymode = await _context.PayModes.FindAsync(id);
+
+            if (paymode != null)
+            {
+                PayMode = paymode;
+                _context.PayModes.Remove(paymode);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToPage("./Index");
+        }
+    }
+}
